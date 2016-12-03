96f764712708261da46964f175098546f57bc262
Ardhio Wibowo <ardhio.sutjipto@mitrais.com>
2016-12-03 21:57:57 +0800

    Initial command line app

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hey! What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}!", name);
+        }
+    }
+}
